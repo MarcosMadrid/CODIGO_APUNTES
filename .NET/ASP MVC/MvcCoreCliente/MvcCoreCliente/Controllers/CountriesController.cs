0684b5922e8318f548226d70417ecadea4b28a85
MarcosMadrid <marquetes130@gmail.com>
2024-04-02 14:22:24 +0200

    fds

---
+++ .NET/ASP MVC/MvcCoreCliente/MvcCoreCliente/Controllers/CountriesController.cs
@@ -0,0 +1,28 @@
+﻿using CountryInfoService;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreCliente.Services;
+
+namespace MvcCoreCliente.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private ServicesCountries servicesCountries;
+
+        public CountriesController(ServicesCountries servicesCountries)
+        {
+            this.servicesCountries = servicesCountries;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            tCountryCodeAndName[] countries = await servicesCountries.GetCountries();
+            return View(countries);
+        }
+
+        public async Task<IActionResult> Details(string isoCode)
+        {
+            tCountryInfo countryInfo = await servicesCountries.GetCountryInfoISO(isoCode);
+            return View(countryInfo);
+        }
+    }
+}
