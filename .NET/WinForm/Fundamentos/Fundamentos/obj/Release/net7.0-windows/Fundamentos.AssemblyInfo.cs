a1866d380cd66381b24806f6cb96ff3f041ddf6e
MarcosMadrid <marquetes130@gmail.com>
2024-04-11 16:39:25 +0200

    ff

--- .NET/WinForm/Fundamentos/Fundamentos/obj/Release/net7.0-windows/Fundamentos.AssemblyInfo.cs
+++ .NET/WinForm/Fundamentos/Fundamentos/obj/Release/net7.0-windows/Fundamentos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8a2380cc07868a74463660a065a3f895c4875c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fcc073d2f8993cc165efd59415661dcbd440250")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
