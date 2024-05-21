0e36d3d881dc3bc4c2912951085d37a6d0b3bdc1
Marcos Madrid Béjar <marcos.madrid@tajamar365.com>
2024-05-21 13:36:38 +0200

    Ok

---
+++ .NET/ElastiCache AWS/MvcCoreElastiCacheAWS/MvcCoreElastiCacheAWS/Controllers/CochesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreElastiCacheAWS.Repositories;
+
+namespace MvcCoreElastiCacheAWS.Controllers
+{
+    public class CochesController : Controller
+    {
+        private RepositoryCoches repositoryCoches;
+
+        public CochesController(RepositoryCoches repositoryCoches)
+        {
+            this.repositoryCoches = repositoryCoches;
+        }
+
+        // GET: CochesController
+        public ActionResult Index()
+        {
+            return View(repositoryCoches.GetCoches());
+        }
+
+        // GET: CochesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(repositoryCoches.GetCoche(id));
+        }
+    }
+}
